b1f934b96418ec6e79a08cb7e4407e2526ff5b4d
Andrey Fidrya <findhostcoupons@gmail.com>
2025-05-14 14:11:54 +0300

    displaying films on the homepage

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Film;
+
+class HomeController extends Controller
+{
+    public function index(){
+    
+    $publishedfilms = Film::all()->where('publication_status', 'published');
+    return view('home',compact('publishedfilms'));
+}
+}
